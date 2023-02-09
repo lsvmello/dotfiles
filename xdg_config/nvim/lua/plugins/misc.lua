10e52156f6794887f55b97b916a924aa3578504f
Leonardo Mello <lsvmello@gmail.com>
2023-02-09 14:40:38 -0300

    misc: added animation

--- xdg_config/nvim/lua/plugins/misc.lua
+++ xdg_config/nvim/lua/plugins/misc.lua
@@ -10,38 +10,50 @@ return {
       -- setup randomizer
       math.randomseed(os.time())
 
-      local race_animation = {
-        fps = 10,
-        name = "race",
-        update = function(grid)
-          for i = 1, #grid do
-            local prev = grid[i][#grid[i]]
-            if math.random(10) % 7 ~= 0 then
+      local animations = {
+        {
+          name = "race",
+          update = function(grid)
+            for i = 1, #grid do
+              local prev = grid[i][#grid[i]]
+              if math.random(10) % 7 ~= 0 then
+                for j = 1, #grid[i] do
+                  grid[i][j], prev = prev, grid[i][j]
+                end
+              end
+            end
+            return true
+          end,
+        },
+        {
+          name = "slide",
+          update = function(grid)
+            for i = 1, #grid do
+              local prev = grid[i][#grid[i]]
               for j = 1, #grid[i] do
                 grid[i][j], prev = prev, grid[i][j]
               end
             end
-          end
-          return true
-        end,
-      }
-      local slide_animation = {
-        fps = 50,
-        name = "slide",
-        update = function(grid)
-          for i = 1, #grid do
-            local prev = grid[i][#grid[i]]
-            for j = 1, #grid[i] do
-              grid[i][j], prev = prev, grid[i][j]
+            return true
+          end,
+        },
+        {
+          name = "shuffle",
+          update = function(grid)
+            for i = 1, #grid do
+              local idx1 = math.random(#grid[i])
+              local idx2 = math.random(#grid[i])
+              grid[i][idx1], grid[i][idx2] = grid[i][idx2], grid[i][idx1]
             end
-          end
-          return true
-        end,
+            return true
+          end,
+        },
       }
 
-      local cellular_automaton = require("cellular-automaton")
-      cellular_automaton.register_animation(race_animation)
-      cellular_automaton.register_animation(slide_animation)
+      -- register all animations
+      for i = 1, #animations do
+        require("cellular-automaton").register_animation(animations[i])
+      end
     end,
   },
 }
