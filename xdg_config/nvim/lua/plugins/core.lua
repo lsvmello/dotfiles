b5cff06cc5a16e69e35e43ee63db378161e66737
Leonardo Mello <lsvmello@gmail.com>
2023-02-10 14:00:51 -0300

    added plenary test keymap

--- xdg_config/nvim/lua/plugins/core.lua
+++ xdg_config/nvim/lua/plugins/core.lua
@@ -1,5 +1,12 @@
 return {
-  { "nvim-lua/plenary.nvim", lazy = false, priority = 9999 },
+  {
+    "nvim-lua/plenary.nvim",
+    lazy = false,
+    priority = 9999,
+    keys = {
+      { "<leader>t", "<Plug>PlenaryTestFile<cr>", desc = "Plenary Test File" },
+    },
+  },
   { "folke/lazy.nvim", version = false },
   {
     "dstein64/vim-startuptime",
