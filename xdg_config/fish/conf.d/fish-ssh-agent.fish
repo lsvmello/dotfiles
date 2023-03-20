b1f2a8ec7c5ed29af20d245513eaba1298fb0993
Leonardo Mello <lsvmello@gmail.com>
2023-03-20 19:40:09 -0300

    added fish utils

---
+++ xdg_config/fish/conf.d/fish-ssh-agent.fish
@@ -0,0 +1,29 @@
+function __ssh_agent_is_started --description "Check if ssh agent is already started"
+  if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
+    source $SSH_ENV > /dev/null
+  end
+
+  if begin; test -z "$SSH_AGENT_PID"; and test -z "$SSH_CONNECTION"; end
+    return 1
+  end
+
+  ssh-add -l > /dev/null 2>&1
+  if test $status -eq 2
+    return 1
+  end
+end
+
+function __ssh_agent_start --description "Start a new ssh agent"
+  ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+  chmod 600 $SSH_ENV
+  source $SSH_ENV > /dev/null
+end
+
+# Start ssh-agent if not started yet, or uses already started ssh-agent
+if test -z "$SSH_ENV"
+    set -xg SSH_ENV $HOME/.ssh/environment
+end
+
+if not __ssh_agent_is_started
+    __ssh_agent_start
+end
