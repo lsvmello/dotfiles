5addbf015b33747be21f314474b402904e2ea7d7
Leonardo Mello <lsvmello@gmail.com>
2023-03-31 16:22:11 -0300

    uses `git_files` and fallback to `find_files` in case of error"

--- xdg_config/nvim/lua/plugins/telescope.lua
+++ xdg_config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,13 @@
 local telescope_builtin = function(builtin, opts)
+  if builtin == "files" then
+    return function()
+      local ok = pcall(require("telescope.builtin").git_files, opts)
+      if not ok then
+        require("telescope.builtin").find_files(opts)
+      end
+    end
+  end
+
   return function()
     require("telescope.builtin")[builtin](opts)
   end
@@ -41,7 +50,7 @@ return {
       { "<leader>/", telescope_builtin( "current_buffer_fuzzy_find"), desc = "[/] Fuzzily search in current buffer]" },
       { "<leader>fc", telescope_builtin("command_history"), desc = "[F]ind [C]ommand History" },
       { "<leader>fd", telescope_builtin("diagnostics"), desc = "[F]ind [D]iagnostics" },
-      { "<leader>ff", telescope_builtin("find_files"), desc = "[F]ind [F]iles" },
+      { "<leader>ff", telescope_builtin("files"), desc = "[F]ind [F]iles" },
       { "<leader>fg", telescope_builtin("live_grep"), desc = "[F]ind by [G]rep" },
       { "<leader>fh", telescope_builtin("help_tags"), desc = "[F]ind [H]elp Tags" },
       { "<leader>fk", telescope_builtin("keymaps"), desc = "[F]ind [K]eymaps" },
