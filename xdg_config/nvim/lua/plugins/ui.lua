6bb1dd47d0899aac0c71e2ac4b9042f65b6aedad
Leonardo Mello <lsvmello@gmail.com>
2023-08-28 13:18:12 -0300

    add `nvim-tree`

--- xdg_config/nvim/lua/plugins/ui.lua
+++ xdg_config/nvim/lua/plugins/ui.lua
@@ -12,6 +12,7 @@ return {
         fidget = true,
         harpoon = true,
         mason = true,
+        nvimtree = true,
         treesitter_context = true,
       },
     },
@@ -19,6 +20,52 @@ return {
       require("catppuccin").setup(opts)
       vim.cmd.colorscheme("catppuccin")
     end,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      view = {
+        number = true,
+        relativenumber = true,
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * 0.5
+            local window_h = screen_h * 0.8
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+              border = "rounded",
+              relative = "editor",
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+        width = function()
+          return math.floor(vim.opt.columns:get() * 0.5)
+        end,
+      },
+    },
+    -- stylua: ignore
+    cmds = {
+      "NvimTreeClipboard", "NvimTreeClose", "NvimTreeCollapse",
+      "NvimTreeCollapseKeepBuffers", "NvimTreeFindFile",
+      "NvimTreeFindFileToggle", "NvimTreeFocus", "NvimTreeOpen",
+      "NvimTreeRefresh", "NvimTreeResize", "NvimTreeToggle",
+    },
+    keys = {
+      { "<Leader>t", "<Cmd>NvimTreeToggle<CR>", desc = "NvimTree Toggle" },
+    },
   },
   {
     "nvim-lualine/lualine.nvim",
