aedc65413632de737bcb8a568625a99cff17fd23
Leonardo Mello <lsvmello@gmail.com>
2024-02-08 20:25:49 -0300

    nvim: refactored `util.lua`

--- xdg_config/nvim/lua/plugins/core.lua
+++ xdg_config/nvim/lua/plugins/core.lua
@@ -1,4 +1,11 @@
 return {
   { "nvim-lua/plenary.nvim", lazy = false, priority = 9999 },
   { "folke/lazy.nvim", version = false },
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
 }
--- xdg_config/nvim/lua/plugins/editor.lua
+++ xdg_config/nvim/lua/plugins/editor.lua
@@ -53,4 +53,22 @@ return {
       })
     end,
   },
+  {
+    "mbbill/undotree",
+    -- stylua: ignore
+    cmd = {
+      "UndotreeFocus", "UndotreeHide",
+      "UndotreeShow", "UndotreeToggle",
+    },
+    keys = {
+      {
+        "<Leader>u",
+        function()
+          vim.cmd.UndotreeToggle()
+          vim.cmd.UndotreeFocus()
+        end,
+        desc = "UndoTree toggle",
+      },
+    },
+  },
 }
