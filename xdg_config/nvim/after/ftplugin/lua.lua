2d36df33defc071deee3e3458f66412ea157356f
Leonardo Mello <lsvmello@gmail.com>
2025-03-11 16:39:20 -0300

    neovim: simplified blinking on execute

--- xdg_config/nvim/after/ftplugin/lua.lua
+++ xdg_config/nvim/after/ftplugin/lua.lua
@@ -1,38 +1,20 @@
 vim.opt_local.commentstring = '-- %s'
 
--- TODO: remove flash_hl and use new timeout on vim.hl.range
-local flash_hl_ns = vim.api.nvim_create_namespace('flash_hl_ns')
-local flash_hl_timer --- @type uv.uv_timer_t?
-local flash_hl_cancel_fn --- @type fun()?
+local blink_ns = vim.api.nvim_create_namespace('blink_ns')
 
-local function flash_hl(start, finish)
+local function blink(start, finish)
   local bufnr = vim.api.nvim_get_current_buf()
-  local winid = vim.api.nvim_get_current_win()
-  if flash_hl_timer then
-    flash_hl_timer:close()
-    assert(flash_hl_cancel_fn)
-    flash_hl_cancel_fn()
-  end
 
-  vim.api.nvim__ns_set(flash_hl_ns, { wins = { winid } })
-  vim.hl.range(bufnr, flash_hl_ns, 'IncSearch', start, finish, {
+  vim.hl.range(bufnr, blink_ns, 'IncSearch', start, finish, {
     regtype = 'V',
     inclusive = true,
+    timeout = 150,
   })
-
-  flash_hl_cancel_fn = function()
-    flash_hl_timer = nil
-    flash_hl_cancel_fn = nil
-    pcall(vim.api.nvim_buf_clear_namespace, bufnr, flash_hl_ns, 0, -1)
-    pcall(vim.api.nvim__ns_set, { wins = {} })
-  end
-
-  flash_hl_timer = vim.defer_fn(flash_hl_cancel_fn, 150)
 end
 
 vim.keymap.set('n', 'g==', function()
   vim.cmd({ cmd = 'lua', range = { vim.fn.line('.') } })
-  flash_hl('.', '.')
+  blink('.', '.')
 end, { buffer = true, desc = 'Execute current line' })
 
 vim.keymap.set('v', 'g=', function()
@@ -44,10 +26,10 @@ vim.keymap.set('v', 'g=', function()
 
   vim.cmd({ cmd = 'lua', range = { line_start, line_end } })
   vim.fn.feedkeys('', 'n')
-  flash_hl("'<", "'>")
+  blink("'<", "'>")
 end, { buffer = true, desc = 'Execute current selection' })
 
 vim.keymap.set('n', 'g=g', function()
   vim.cmd.source('%')
-  flash_hl({ 1, 1 }, '$')
-end, { buffer = true, desc = 'Source current file' })
+  blink({ 1, 1 }, '$')
+end, { buffer = true, desc = 'Execute current file' })
